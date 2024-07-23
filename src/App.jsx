5c7fd17933d2ee8e72fb6b0ab953d3049185119e
Maor Levi <lmaor7788@gmail.com>
2024-07-23 10:10:41 +0300

    page-updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/page" element={<Page />} />
 
           <Route path="/" element={<Validation />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 }

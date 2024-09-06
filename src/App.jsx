86d92c22cc000721e6174feee6ed35cc003f9fb0
Maor Levi <lmaor7788@gmail.com>
2024-09-06 13:55:49 +0300

    empty-project

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
           <Route path="/" element={<Validation />} />
         </Routes>
       </BrowserRouter> */}
-      <div>אתה רוצה את האתר , אתה מוזמן להתקשר למס׳ הבא : 050-544-2056</div>
     </div>
   );
 }
